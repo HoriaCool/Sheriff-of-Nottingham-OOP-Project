c024653a6b3266a7904f8f5dd077d82412ee98c7
FacCeVreauDin99 <nedelcu.horia.alexandru@gmail.com>
2019-12-01 00:39:43 +0200

    new feature

---
+++ main/Main.java
@@ -0,0 +1,24 @@
+// Copyright 2019 Nedelcu Horia (nedelcu.horia.alexandru@gmail.com)
+
+package main;
+
+public final class Main {
+    private Main() {
+        // just to trick checkstyle
+    }
+
+    public static void main(final String[] args) {
+        /*
+        GameInputLoader gameInputLoader = new GameInputLoader(args[0], args[1]);
+        GameInput gameInput = gameInputLoader.load();
+
+        if (gameInput.isValidInput()) {
+            GoodsFactory goodsFactory = GoodsFactory.getInstance();
+            GameLogic gameLogic = new GameLogic(gameInput);
+            gameLogic.gameStart();
+        } else {
+            System.out.println("error: Bad Input!");
+        }
+        */
+    }
+}
